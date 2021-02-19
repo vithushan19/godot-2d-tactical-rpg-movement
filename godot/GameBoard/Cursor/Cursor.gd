072996ef82c6e79fdae9b8b1d3c943bf4ede3e90
vithushan19 <vithushan19@gmail.com>
2021-02-19 15:12:22 -0500

    Revert "make cursor the player"
    This reverts commit 25ac7de479b5bf5e45b4b1dfb4e7f2728220206c.

--- godot/GameBoard/Cursor/Cursor.gd
+++ godot/GameBoard/Cursor/Cursor.gd
@@ -26,8 +26,11 @@ func _ready() -> void:
 
 
 func _unhandled_input(event: InputEvent) -> void:
+	# Navigating cells with the mouse.
+	if event is InputEventMouseMotion:
+		self.cell = grid.calculate_grid_coordinates(event.position)
 	# Trying to select something in a cell.
-	if event.is_action_pressed("click") or event.is_action_pressed("ui_accept"):
+	elif event.is_action_pressed("click") or event.is_action_pressed("ui_accept"):
 		emit_signal("accept_pressed", cell)
 		get_tree().set_input_as_handled()
 
